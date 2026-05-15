66bd226e09bd02e2f01f8cdfbadf911f437cb76e
hwjsldgj <905798451@qq.com>
2026-05-16 00:02:33 +0800

    将原始代码写入

---
+++ moya/MoyaCode/Powers/MoyaPowers.cs
@@ -0,0 +1,38 @@
+using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using Godot;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MoeNegiMod.Moya.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoeNegiMod.Moya.Powers;
+
+public abstract class MoyaPowers : CustomPowerModel
+{
+    public override string CustomPackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "power.png".PowerImagePath(); 
+        }
+    }
+
+    public override string CustomBigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "power.png".BigPowerImagePath();
+        }
+    }
+
+   
+
+    
+    
+}
